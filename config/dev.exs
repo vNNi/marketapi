985e80a4781c30a33a9e158107dd19eeac0bc0ea
Vinicius Komninakis <vinicius.komninakis@luizalabs.com>
2019-10-26 12:54:09 -0300

    chore: add utf8

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :marketapi, Marketapi.Repo,
   password: "12345",
   database: "market",
   hostname: "localhost",
+  charset: "utf8",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
